cf8f757d99893e1560b2774f606ab455a9958fa1
User <pavadi.abhishek@gmail.com>
2023-08-30 20:17:29 -0700

    MailSend using rest api Initial commit

---
+++ src/main/java/com/example/mailsendrestapii/MailSendRestApiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailsendrestapii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSendRestApiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSendRestApiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mailsendrestapii/MailSendRestApiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailsendrestapii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendRestApiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
